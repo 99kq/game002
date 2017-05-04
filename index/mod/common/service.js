c89063806ab875df78fc5c641c29181843ed3bab
vitrum.zhu <vitrum.zhu@99bill.com>
2017-05-04 11:24:02 +0800

    调整逻辑

--- index/mod/common/service.js
+++ index/mod/common/service.js
@@ -16,12 +16,8 @@ if (location.host === 'sandbox.99bill.com') {
 var api = {
   validateCode: ajax.baseUrl.ebd + '/base/3.0/sms/validateCode', //C0010001N 生成MAM短信验证码
   register: ajax.baseUrl.ebd + '/mam/3.0/members', //L0020002N 注册快钱账号3.0
-  auth: ajax.baseUrl.ebd + '/auth/3.0/billApi', //L0070005N bill-api-code登录授权校验v1.1
-  holdVoucher: ajax.baseUrl.rip + '/api/activities/v1/holdVoucher', //T0040010 领券接口
-  checkVoucher: ajax.baseUrl.rip + '/api/activities/v1/checkVoucher', //T0040010 检查券接口
-  //  https://rip.99bill.com/rip-website/api/activities/v1/checkVoucher
-  // 绑定常客和快钱账号接口
-  addCardRecord: ajax.baseUrl.ebdvas + '/card/h5/addCardRecord', //E00010001L卡数据落库
+  rewardRequest: ajax.baseUrl.ebdvas + '/activity/reward/rewardRequest', //宝箱开启结果接口 抽奖接口
+  activityRemainInfo: ajax.baseUrl.ebdvas + '/activity/reward/activityRemainInfo', //获取抽奖活动剩余时间和次数情况
 };
 
 
@@ -29,25 +25,6 @@ var method = Dom7.api;
 var pubData = JSON.stringify({"c": "H5", "b": "CSD-KYH-GU", "id": "100", "t": new Date()/1 + ''});
 
 module.exports = {
-  //L0070005N bill-api-code登录授权校验
-  auth : function(opt){
-    opt = {
-      url: {
-          // mock: 'joinAct.json',
-          real: api.auth
-      },
-      headers: {
-          pubData: pubData
-      },
-      method: 'POST',
-      data: opt.data,
-      jsonType: true,
-      success: opt.success,
-      code: 'diy',
-      comment: '登录授权校验'
-    };
-    ajax.fn(opt);
-  },
   //C0010001N 生成MAM短信验证码
   validateCode: function(opt) {
     opt = {
@@ -88,50 +65,11 @@ module.exports = {
 
     ajax.fn(opt);
   },
-  // T0040010 检查券接口
-  checkVoucher: function(opt){
+  //抽奖
+  rewardRequest: function(opt){
     opt = {
       url: {
-          // mock: 'joinAct.json',
-          real: api.checkVoucher
-      },
-      headers: {
-          Authorization: sessionStorage.loginToken
-      },
-      method: 'GET',
-      data: opt.data,
-      jsonType: true,
-      success: opt.success,
-      code: 'diy',
-      comment: '检查券'
-    };
-    ajax.fn(opt);
-  },
-  //T0040010 领券接口
-  holdVoucher: function(opt){
-    opt = {
-      url: {
-          // mock: 'joinAct.json',
-          real: api.holdVoucher
-      },
-      headers: {
-          authorization: sessionStorage.loginToken
-      },
-      method: 'GET',
-      data: opt.data,
-      jsonType: true,
-      success: opt.success,
-      code: 'diy',
-      comment: '领券'
-    };
-    ajax.fn(opt);
-  },
-  //E00010001L卡数据落库
-  bindAccount: function(opt){
-    opt = {
-      url: {
-          // mock: 'joinAct.json',
-          real: api.addCardRecord
+          real: api.rewardRequest
       },
       headers: {
           authorization: sessionStorage.loginToken
@@ -142,6 +80,24 @@ module.exports = {
       success: opt.success,
       code: 'diy',
       comment: '领券'
+    };
+    ajax.fn(opt);
+  },
+  //
+  activityRemainInfo: function(opt){
+    opt = {
+      url: {
+          real: api.activityRemainInfo
+      },
+      headers: {
+          authorization: sessionStorage.loginToken
+      },
+      method: 'POST',
+      data: opt.data,
+      jsonType: true,
+      success: opt.success,
+      code: 'diy',
+      comment: '查状态 '
     };
     ajax.fn(opt);
   }
--- index/mod/index/index.js
+++ index/mod/index/index.js
@@ -1,55 +1,116 @@
 import {Component} from 'react'
 import service from '../common/service'
 import tongji from '../common/tongji'
+import util from '../common/util'
 import {Motion, spring} from 'react-motion';
 
+
+// 设定箱子晃动动画弹性
 const springSetting1 = {stiffness: 600, damping: 1, precision:0.01};
-
-
-function clamp(n, min, max) {
-  return Math.max(Math.min(n, max), min);
-}
 
 class Home extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      openStatus: false,
+      openStatus: false, //当前箱子开启状态
       roke: false,
-      leftTime:60
+      leftTime: 60,
+      activityInfoId:'', //活动id
+      countdown:19999,  //活动剩余时间或冷却时间	number	秒为单位，倒计时
+      status:"00",  //00 为开始，01当日已开完， 02有冷却中宝箱， 03有可开启宝箱，99活动已结束, 98为未开始
+      todayAll:3, //今天可开数	number	今天进行了一次抽奖，就会-1，这次活动第一次查询返回的是3
+      todayOpened:0,   //今天已开数	number	进行了一次抽奖就会+1
+      timeInterval:10 // 抽取宝箱的间隔时间
     };
-  };
+  }
 
   componentDidMount() {
+    this.requestEventStatus();
     this.timer = setInterval(function () {
-        var count = this.state.leftTime;
+        let count = this.state.leftTime;
+        let countdown = this.state.countdown;
         count -= 1;
         if (count < 1) {
           count = 60;
         }
-        let y = 3 ;
+        this.setState({
+          leftTime: count
+        });
+        //倒计时计算箱子是否可以开启
+        if ( countdown > 0 ){
+          this.setState({
+              countdown : countdown-1
+          });
+        }else if(this.state.todayAll>0){
+          this.setState({
+              openStatus : false
+          });
+        }
+        //每间隔5秒箱子晃动一次
+        let y = 5 ; 
         let z = parseInt(count / y);
         if ( y*z == count){
           this.handleRockBox();
         }
-        this.setState({
-          leftTime: count
-        });
     }.bind(this), 1000);
-  };
+  }
+  //获取活动信息
+  requestEventStatus = () => {
+    let that = this;
+    let infoData = {};
+    service.activityRemainInfo({
+      data: infoData,
+      success: function(infoRes) {
+        if(infoRes.responseCode === '00'){
+          // 保存活动信息
+          that.setState({
+            'activityId':infoRes.activityId,
+            'countdown':infoRes.countdown,
+            'status':infoRes.status,
+            'todayAll':infoRes.todayAll,
+            'todayOpened':infoRes.todayOpened,
+          })
+        }else{
+          app.alert('获取活动信息:'+infoRes.responseMessagev);
+        }
+      }
+    });
+  }
+  // 
+  requestEventReward = () => {
+
+  }
   //打开盒子
   handleOpenBox = () => {
     console.log('handleOpenBox',this.state);
-    if(this.state.openStatus){
-      console.log("已开，需要等10分钟后开启！！"); 
+    let boxNmuber = this.state.todayAll;
+    //倒计时
+    if(this.state.countdown>0){
+      let sec_num = parseInt(this.state.countdown, 10)
+      let hours   = Math.floor(sec_num / 3600)
+      let minutes = Math.floor((sec_num - (hours * 3600)) / 60)
+      let seconds = sec_num - (hours * 3600) - (minutes * 60)
+      if (minutes < 10) {minutes = "0" + minutes}
+      if (seconds < 10) {seconds = "0" + seconds}
+      app.alert("需要等"+ minutes +"分"+seconds+"秒后开启！！"); 
+      return false;
     }else{
-      this.setState({'openStatus': true});
+      // 有可开箱子
+      if( boxNmuber> 0){
+        this.setState({
+          'openStatus': true,
+          'todayAll': boxNmuber - 1,
+          'countdown': this.state.timeInterval
+        });
+      }else{
+        app.alert("已开达到今天上限，明天再来！！"); 
+      }
     }
-  };
+  }
   // 晃动动画
   handleRockBox = () => {
-    console.log('handleRockBox',this.state);
+    console.log('handleRockBox');
     this.setState({'roke': !this.state.roke});
   }
   render() {
@@ -59,25 +120,60 @@ class Home extends Component {
     }else{
        boxClaseName = 'chest';
     }
+    // 剩余时间倒计时
+    function setLeftTime(seconds_in){
+        if (isNaN(seconds_in)) {
+            return '00:00:00'
+          } else {
+            let sec_num = parseInt(seconds_in, 10)
+            let hours   = Math.floor(sec_num / 3600)
+            let displayHours = Math.floor(sec_num / 3600)%24
+            let days    = Math.floor(hours / 24)
+            let minutes = Math.floor((sec_num - (hours * 3600)) / 60)
+            let seconds = sec_num - (hours * 3600) - (minutes * 60)
+            if (hours   < 10) {hours   = "0" + hours}
+            if (minutes < 10) {minutes = "0" + minutes}
+            if (seconds < 10) {seconds = "0" + seconds}
+            if (days < 1){ days = ""}
+            return (
+                <div className="left-text">
+                   <span>剩余时间</span>  {days }<span>天</span> {displayHours}<span>时</span> {minutes}<span>分</span> {seconds}<span>秒</span>
+                </div>
+            )
+          }
+    }
     return (
       <div className="lottery">
+        <div className="headbox">
+          <div className="status">
+            {this.state.status}
+          </div>
+          <div className="info">
+            每天瓜分1000万
+            100件3C爆款免息购
+            今天你有3次机会
+          </div>
+        </div>
         <div className="chestbox">
           <Motion style={{scale: spring(this.state.roke ? 1.02 : 1, springSetting1)}}>
             {({scale}) =>
               <div className={boxClaseName} style={{
                 WebkitTransform: ` scale(${scale})`,
                 transform: `scale(${scale})`
-              }} onClick={this.handleOpenBox.bind()} />
+              }} onClick={this.handleRockBox.bind()} />
             }
           </Motion>
         </div>
         <div className="btnbox">
-          <a href="#" className="btn" onClick={this.handleOpenBox.bind()}>开箱子</a>
-          <a href="#" className="btn" onClick={this.handleRockBox.bind()}>晃箱子</a>
+          <a href="#" className="btn" onClick={this.handleOpenBox.bind()}>点击开宝箱</a>
+        </div>
+        <div className="footer">
+          <div className="txt">距下次宝箱开启时间：</div>
+          <div className="time">{setLeftTime.bind(this,this.state.countdown)()}</div>
         </div>
       </div>
     );
-  };
+  }
   
 }
 
